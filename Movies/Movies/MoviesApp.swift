cc5c907e78c96cdbce0c6aa16e75e443f6e6be39
Yulia Aleekseeva <yuvodneva@gmail.com>
2024-10-18 11:59:03 +0300

    initial commit

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Yuliya Vodneva on 18.10.24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
